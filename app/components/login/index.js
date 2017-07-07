f535ebbf19a8738a96767aa49c4c2aae31affc15
JMEPJ\PC <1485165003@qq.com>
2017-07-07 14:51:54 +0800

    登录注册过程，是否需要加验证码待商议，表单验证明天做

--- app/components/login/index.js
+++ app/components/login/index.js
@@ -3,16 +3,41 @@
  */
 
 import React, {Component} from 'react';
+import {Col,Form,FormGroup,ControlLabel,ButtonGroup,FormControl,Button} from 'react-bootstrap'
 
-import  './index.css'
+import  './index.scss'
+import '../common.scss'
 
-
+import Header from '../headers/index';
 export default class LoginIn extends Component{
 
     render(){
         return(
-            <div className='top'>
-               登录界面
+            <div>
+               <Header/>
+                <div  className="general-background-color">
+                <Col sm={8} md={8} mdOffset={2}>
+                  <div className="login-case">
+                     <h2 className="login-theme">欢迎登录youtome</h2>
+                      <div className="login-form">
+                          <Form >
+                              <FormGroup controlId="formInlineName">
+                                  <FormControl type="text" placeholder="请输入你的手机号" />
+                              </FormGroup>
+                              {' '}
+                              <FormGroup controlId="formInlineEmail">
+                                  {' '}
+                                  <FormControl type="text" placeholder="请输入密码" />
+                              </FormGroup>
+                              {' '}
+                              <ButtonGroup vertical block>
+                                  <Button type="submit" bsStyle="primary">登录</Button>
+                              </ButtonGroup>
+                          </Form>
+                      </div>
+                  </div>
+                </Col>
+                </div>
             </div>
         )
     }
--- app/components/register/index.js
+++ app/components/register/index.js
@@ -2,16 +2,47 @@
  * Created by PC on 2017/7/6.
  */
 
-import React ,{Component} from 'react';
+import React, {Component} from 'react';
+import {Col,Form,FormGroup,ControlLabel,ButtonGroup,FormControl,Button} from 'react-bootstrap'
 
+import  './index.scss'
+import '../common.scss'
 
+import Header from '../headers/index';
 export default class Register extends Component{
 
     render(){
         return(
             <div>
-                我是注册界面，666666666666
+                <Header/>
+                <div  className="general-background-color">
+                    <Col sm={8} md={8} mdOffset={2}>
+                        <div className="login-case">
+                            <h2 className="login-theme">欢迎注册youtome</h2>
+                            <div className="login-form">
+                                <Form >
+                                    <FormGroup controlId="formInlineName">
+                                        <FormControl type="text" placeholder="请输入你的手机号" />
+                                    </FormGroup>
+                                    {' '}
+                                    <FormGroup controlId="formInlineEmail">
+                                        {' '}
+                                        <FormControl type="text" placeholder="请输入密码" />
+                                    </FormGroup>
+                                    {' '}
+                                    <FormGroup controlId="formInlineName">
+                                        <FormControl type="text" placeholder="请再次输入你的密码" />
+                                    </FormGroup>
+                                    {' '}
+                                    <ButtonGroup vertical block>
+                                        <Button type="submit" bsStyle="primary">注册</Button>
+                                    </ButtonGroup>
+                                </Form>
+                            </div>
+                        </div>
+                    </Col>
+                </div>
             </div>
-            )
+        )
     }
 }
